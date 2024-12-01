6588e08db0f85a33bd483cf8c4b9ce7a53dfd207
Torkieom <tomasz.szymanski.op@gmail.com>
2024-12-01 16:45:02 +0100

    Tworzenie pliku i czytanie hasła

--- Zadanie Domowe 7 - Laba/ZadDom6.cpp
+++ Zadanie Domowe 7 - Laba/ZadDom6.cpp
@@ -1,10 +1,58 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 
+using namespace std;
+
+class createFile
+{
+public:
+
+    void createFileWithPassword(const string & password)
+    {
+        ofstream file("haslo.txt");
+        if (file.is_open())
+        {
+            file << password;
+            file.close();
+        }
+        else
+        {
+            cout << "Nie mozna utworzyc pliku" << endl;
+        }
+    }
+
+    string readPasswordFromFile()
+    {
+        ifstream file("haslo.txt");
+        if (file.is_open())
+        {
+            getline(file, password);
+            file.close();
+        }
+        else
+        {
+            cout << "Nie mozna odczytac pliku" << endl;
+        }
+        return password;
+    }
+
+private:
+    string password;
+
+};
+
+
 int main()
 {
-	ss
+	createFile plik;
+	string hasloPlik = "przyklad";
+    plik.createFileWithPassword(hasloPlik);
+    string hasloGra = plik.readPasswordFromFile();
+
+    cout << hasloGra;
+
 	return 0;
 }
